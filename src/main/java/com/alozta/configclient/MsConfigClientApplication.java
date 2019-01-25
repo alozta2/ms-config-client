5f263f2aa9e906f66dc471eeaff81bdae34621ea
Ozan Tacalan <alozta@gmail.com>
2019-01-25 23:08:49 +0300

    Initial commit

---
+++ src/main/java/com/alozta/configclient/MsConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.alozta.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigClientApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/alozta/configclient/MsConfigClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.alozta.configclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsConfigClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
